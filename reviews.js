7436b291ee8d37d53c07b1f52a0b79145c30e9c2
Andrey Roshchupkin <reivrn@gmail.com>
2023-08-16 15:09:10 +0400

    clinics revшуцы add

--- reviews.js
+++ reviews.js
@@ -1,1 +1,53 @@
-export const clinicsReviews = [];
+export const clinicsReviews = [
+  {
+    clinic_id: "CLINIC001",
+    reviews: [
+      {
+        patient_id: "PATIENT001",
+        phone_number: "+374-41-720001",
+        date_of_review: "2023-08-16",
+        date_of_visit: "2023-08-10",
+        text_of_review:
+          "I had a great experience at this clinic. The staff was friendly and the facilities were clean.",
+        rating: 4,
+      },
+      {
+        patient_id: "PATIENT002",
+        phone_number: "+374-41-720002",
+        date_of_review: "2023-08-15",
+        date_of_visit: "2023-08-12",
+        text_of_review:
+          "I was quite satisfied with the service I received. The doctor was knowledgeable and attentive.",
+        rating: 5,
+      },
+    ],
+  },
+  {
+    clinic_id: "CLINIC002",
+    reviews: [
+      {
+        patient_id: "PATIENT003",
+        phone_number: "+374-41-720003",
+        date_of_review: "2023-08-14",
+        date_of_visit: "2023-08-09",
+        text_of_review:
+          "I had a positive experience at this clinic. The treatment I received was effective.",
+        rating: 4,
+      },
+    ],
+  },
+  {
+    clinic_id: "CLINIC003",
+    reviews: [
+      {
+        patient_id: "PATIENT004",
+        phone_number: "+374-41-720004",
+        date_of_review: "2023-08-13",
+        date_of_visit: "2023-08-11",
+        text_of_review:
+          "I had a disappointing experience at this clinic. The staff was not very helpful.",
+        rating: 2,
+      },
+    ],
+  },
+];
